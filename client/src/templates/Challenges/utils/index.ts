d061a197a191f7b3fbbe54ff870ff5d8a80f75eb
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-10-18 14:19:08 +0700

    feat: add support for TypeScript to enhancePrismAccessibility

--- client/src/templates/Challenges/utils/index.ts
+++ client/src/templates/Challenges/utils/index.ts
@@ -50,7 +50,9 @@ export function enhancePrismAccessibility(
     sql: 'SQL',
     http: 'HTTP',
     json: 'JSON',
-    pug: 'pug'
+    pug: 'pug',
+    typescript: 'TypeScript',
+    ts: 'TypeScript'
   };
   const parent = prismEnv?.element?.parentElement;
   if (
