aa9714ddc38768d44a1778fbe5bd9944fa302c7f
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-10-18 14:18:10 +0700

    chore: clean up PrismFormatted implementation

--- client/src/templates/Challenges/components/prism-formatted.tsx
+++ client/src/templates/Challenges/components/prism-formatted.tsx
@@ -1,40 +1,39 @@
-import Prism from 'prismjs';
-import React, { useRef, useEffect } from 'react';
-import { enhancePrismAccessibility } from '../utils';
+import React from 'react';
+import { PrismFormatted as FccUIPrismFormatted } from '@freecodecamp/ui';
+import { useTranslation } from 'react-i18next';
 
 interface PrismFormattedProps {
   className?: string;
   text: string;
   useSpan?: boolean;
   noAria?: boolean;
+  hasLineNumbers?: boolean;
+  isCollapsible?: boolean;
 }
 
 function PrismFormatted({
   className,
   text,
   useSpan,
-  noAria
+  noAria,
+  isCollapsible
 }: PrismFormattedProps): JSX.Element {
-  const instructionsRef = useRef<HTMLDivElement>(null);
-  const ElementName = useSpan ? 'span' : 'div';
-
-  if (noAria) {
-    text = text.replace(/<pre( [^>]+)?>/, '<pre$1 data-no-aria="true">');
-  }
-
-  useEffect(() => {
-    // Just in case 'current' has not been created, though it should have been.
-    if (instructionsRef.current) {
-      Prism.hooks.add('complete', enhancePrismAccessibility);
-      Prism.highlightAllUnder(instructionsRef.current);
-    }
-  }, []);
+  const { t } = useTranslation();
 
   return (
-    <ElementName
+    <FccUIPrismFormatted
       className={className}
-      dangerouslySetInnerHTML={{ __html: text }}
-      ref={instructionsRef}
+      text={text}
+      noAria={noAria}
+      useSpan={useSpan}
+      getCodeBlockAriaLabel={codeName =>
+        t('aria.code-example', {
+          codeName
+        })
+      }
+      {...(isCollapsible
+        ? { isCollapsible: true, disclosureLabel: t('learn.example-code') }
+        : { isCollapsible: false })}
     />
   );
 }
